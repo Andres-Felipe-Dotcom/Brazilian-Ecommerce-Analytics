1e049641429beeac63226ba0d159edb54a4018c7
Andres-Felipe-Dotcom <anfegucu@gmail.com>
2026-05-13 14:52:06 +0930

    Add files via upload

---
+++ 01_exploration.sql
@@ -0,0 +1,31 @@
+SELECT *
+FROM customers
+LIMIT 5;
+
+SELECT *
+FROM order_payments
+LIMIT 5;
+
+SELECT *
+FROM order_reviews
+LIMIT 5;
+
+SELECT *
+FROM orders
+LIMIT 5;
+
+SELECT *
+FROM order_items
+LIMIT 5;
+
+SELECT *
+FROM products
+LIMIT 5;
+
+SELECT *
+FROM sellers
+LIMIT 5;
+
+SELECT *
+FROM product_category_name_translation
+LIMIT 5;
